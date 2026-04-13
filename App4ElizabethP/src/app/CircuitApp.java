98ef0fd7bcc20616061223b9445b598f8088ee37
2562116 <2562116@cegeplimoilou.ca>
2026-04-13 08:36:14 -0400

    Creation de la méthode recommencer() dans CircuitApp.

--- App4ElizabethP/src/app/CircuitApp.java
+++ App4ElizabethP/src/app/CircuitApp.java
@@ -7,15 +7,17 @@ public class CircuitApp {
 
     private static final char fSep = File.separatorChar;
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        CircuitApp app = new CircuitApp();
         System.out.println("Bonjour, quelle fichier voulez vous calculer? Taper le chiffre correspondant au fichier souhaiter.");
-        choixFichier();
-        //while ()
+        app.choixFichier();
+        app.recommencer();
     }
 
-    public void choixFichier (){
+    public void choixFichier() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("[1] : complexe_industriel_zone_nord.json" + "\n" + "[2] : eclairage_public_quartier.json" + "\n" + "[3] : reseau_secours_hopital.json" );
+        System.out.println("[1] : complexe_industriel_zone_nord.json" + "\n" + "[2] : eclairage_public_quartier.json" + "\n" + "[3] : reseau_secours_hopital.json");
         int choix1 = sc.nextInt();
         CircuitBuilder c = new CircuitBuilder();
         System.out.println(c.construireCircuit(cheminFichier(choix1)).calculerResistance() + "Ω");
@@ -36,5 +38,31 @@ public class CircuitApp {
         }
         String c = System.getProperty("user.dir") + fSep + "App4ElizabethP" + fSep + "src" + fSep + "donnees" + fSep + "fichiers_json" + fSep + s;
         return c;
+    }
+
+    public void recommencer(){
+        Scanner sc = new Scanner(System.in);
+        boolean c;
+        System.out.println("Taper la lettre correspondant à l'action souhaitée." + "\n" + "[R] Tester un autre fichier" + "\n" + "[Q] Quitter");
+        String choix2 = sc.nextLine();
+        if (choix2.equals("R")){
+            c = true;
+        } else if (choix2.equals("Q")) {
+            c = false;
+        }else{
+            throw new IllegalArgumentException();
+        }
+        while (c){
+            choixFichier();
+            System.out.println("Taper la lettre corespondant a l'action souhaité." + "\n" + "[R] Tester un autre fichier" + "\n" + "[Q] Quitter");
+            String choi2 = sc.nextLine();
+            if (choi2.equals("R")){
+                c = true;
+            } else if (choi2.equals("Q")) {
+                c = false;
+            }else{
+                System.out.println("Erreur: entrez seulement R ou Q");
+            }
+        }
     }
 }
